23c76940c93446a877cb63849daed01715918434
ruddmslee <dlruddms326@naver.com>
2025-03-30 03:53:39 +0900

    마이페이지 js 작성

---
+++ week03/mypage.js
@@ -0,0 +1,19 @@
+function modifyPassword() {
+    let currentPw = prompt("현재 비밀번호를 입력하세요");
+    let pw1, pw2;
+    if (currentPw != "" && currentPw != null) {
+        pw1 = prompt("변경할 비밀번호를 입력하세요");
+        if (pw1 != "" && pw1 != null) {
+            pw2 = prompt("비밀번호 확인을 위해 다시 입력해주세요.")
+            while (true) {
+                if (pw1 == pw2 || pw2 == null) {
+                    confirm("비밀번호 변경 성공!");
+                    break;
+                }
+                else {
+                    pw2 = prompt("비밀번호가 일치하지 않습니다. 다시 입력하세요.")
+                }
+            }
+        }
+    }
+}
