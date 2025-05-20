766481f9b8dd3f21bb2479c301402d0b8b746006
ruddmslee <dlruddms326@naver.com>
2025-05-20 20:04:13 +0900

    Feat: @api_view+request body 계산기 구현

--- django/week04/myinsta/urls.py
+++ django/week04/myinsta/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from posts.views_cal import calculator_query
+from posts.views_cal import calculator_query, calculator_body
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls', namespace='posts')),
     path('calculater/query', calculator_query),
+    path('calculater/body', calculator_body),
 ]
