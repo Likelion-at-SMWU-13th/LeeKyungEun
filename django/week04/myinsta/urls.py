db8889f01b05f23dccf18098f434f08de351e1a3
ruddmslee <dlruddms326@naver.com>
2025-05-20 19:59:55 +0900

    Feat: @api_view+quesry param 계산기 구현

--- django/week04/myinsta/urls.py
+++ django/week04/myinsta/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from posts.views_cal import calculator_query
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls', namespace='posts')),
+    path('calculater/query', calculator_query),
 ]
---
+++ django/week04/posts/views_cal.py
@@ -0,0 +1,27 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+#꼭 views.py에 다 넣을 필요 X
+@api_view(['GET'])
+def calculator_query(request):
+    num1 = request.GET.get('num1', 0)  #쿼리 파라미터 가져오기
+    num2 = request.GET.get('num2', 0)
+    op = request.GET.get('op')
+
+    if op == '+':
+        result = int(num1) + int(num2)
+    elif op == '-':
+        result = int(num1) - int(num2)
+    elif op == '*':
+        result = int(num1) * int(num2)
+    elif op == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+
+    data = {
+        'op' : op,
+        'result' : result
+    }
+
+    return Response(data)
