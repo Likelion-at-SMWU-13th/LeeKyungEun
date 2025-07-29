2f88ab7ba04988e646018b1bc93cac8eeba175a6
ruddmslee <dlruddms326@naver.com>
2025-07-29 19:56:42 +0900

    Feat: ProductController 수정

--- spring/week03/seminar2/src/main/java/com/example/seminar2/controllers/ProductController.java
+++ spring/week03/seminar2/src/main/java/com/example/seminar2/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.seminar2.controllers;
 
+import com.example.seminar2.model.Product;
 import com.example.seminar2.service.ProductService;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public class ProductController {
 
@@ -14,6 +18,23 @@ public class ProductController {
 
     @RequestMapping("/products")
     public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+    @RequestMapping(path="/products", method = RequestMethod.POST)
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ) {
+        Product product = new Product();
+        product.setName(name);
+        product.setPrice(price);
+        productService.addProduct(product);
+
         var products = productService.findAll();
         model.addAttribute("products", products);
 
