a6524b2701229f45faac0d4ac177ecc35b3bc23e
ruddmslee <dlruddms326@naver.com>
2025-07-29 19:23:48 +0900

    Feat: 경로 변수 사용

--- spring/week03/seminar/src/main/java/com/example/seminar/controller/MainController.java
+++ spring/week03/seminar/src/main/java/com/example/seminar/controller/MainController.java
@@ -2,16 +2,18 @@ package com.example.seminar.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
-    @RequestMapping("/home")
+    @RequestMapping("/home/{color}")
     public String home(Model page,
-                       @RequestParam(required=false) String color,
-                       @RequestParam(required=false) String name) {
-        page.addAttribute("username", name);
+                       //@RequestParam(required=false) String color,
+                       //@RequestParam(required=false) String name
+                       @PathVariable String color) {
+        page.addAttribute("username", "babylion");
         page.addAttribute("color", color);
         return "home.html";
     }
