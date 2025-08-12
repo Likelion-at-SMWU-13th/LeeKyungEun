c2e409b2548e800e61d423e72890dae999cb02d4
ruddmslee <dlruddms326@naver.com>
2025-08-12 21:19:27 +0900

    Feat: 스프링 데이터베이스 실습

--- spring/week05/seminar/src/main/java/com/example/seminar/entity/PostEntity.java
+++ spring/week05/seminar/src/main/java/com/example/seminar/entity/PostEntity.java
@@ -1,4 +1,28 @@
 package com.example.seminar.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="post")
+@Getter
+@Setter
+@NoArgsConstructor
 public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String content;
+    private String writer;
+
+    @JoinColumn(name="board_id")
+    @ManyToOne(
+            targetEntity = BoardEntity.class,
+            fetch = FetchType.LAZY
+    )
+    private BoardEntity boardEntity;
 }
