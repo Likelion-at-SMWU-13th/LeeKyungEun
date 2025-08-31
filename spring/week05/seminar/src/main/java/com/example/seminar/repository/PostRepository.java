717caaf158d9aed36c7dd1e5bbec2606dc4de368
ruddmslee <dlruddms326@naver.com>
2025-08-31 19:59:07 +0900

    Feat: PostRepository 코드 추가

--- spring/week05/seminar/src/main/java/com/example/seminar/repository/PostRepository.java
+++ spring/week05/seminar/src/main/java/com/example/seminar/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.example.seminar.repository;
 
+import com.example.seminar.entity.BoardEntity;
 import com.example.seminar.entity.PostEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends CrudRepository<PostEntity, Long> {
+    int countByBoardEntity(BoardEntity boardEntity);
+    Optional<List<PostEntity>> findByBoardEntity(BoardEntity boardEntity);
 }
