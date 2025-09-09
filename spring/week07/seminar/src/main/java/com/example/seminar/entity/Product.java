93a70f366a288558b291ca5972ebab0985878914
ruddmslee <dlruddms326@naver.com>
2025-09-09 20:28:29 +0900

    Feat: 다대다 매핑 실습

--- spring/week07/seminar/src/main/java/com/example/seminar/entity/Product.java
+++ spring/week07/seminar/src/main/java/com/example/seminar/entity/Product.java
@@ -1,20 +1,23 @@
 package com.example.seminar.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Product {
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "product")
+public class Product extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long number;
 
     @Column(nullable = false)
     private String name;
@@ -27,11 +30,26 @@ public class Product {
 
     @OneToOne(mappedBy = "product")
     @ToString.Exclude
+    @EqualsAndHashCode.Exclude
     private ProductDetail productDetail;
 
     @ManyToOne
     @JoinColumn(name = "provider_id")
     @ToString.Exclude
     private Provider provider;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Producer> producers = new ArrayList<>();
+
+    public void addProducer(Producer producer){
+        this.producers.add(producer);
+    }
+
+    public Product(String name, Integer price, Integer stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
 
 }
---
+++ spring/week07/seminar/src/main/java/com/example/seminar/repository/ProducerRepository.java
@@ -0,0 +1,7 @@
+package com.example.seminar.repository;
+
+import com.example.seminar.entity.Producer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProducerRepository extends JpaRepository<Producer, Long> {
+}
