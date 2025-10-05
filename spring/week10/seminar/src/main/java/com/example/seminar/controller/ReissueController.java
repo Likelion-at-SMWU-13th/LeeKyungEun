9419aa2cd18f271954d194b71de831169872662f
ruddmslee <dlruddms326@naver.com>
2025-10-05 18:38:00 +0900

    Feat: Access Token 재발급 시에도 Refresh 토큰 저장하도록 수정

--- spring/week10/seminar/src/main/java/com/example/seminar/controller/ReissueController.java
+++ spring/week10/seminar/src/main/java/com/example/seminar/controller/ReissueController.java
@@ -1,5 +1,7 @@
 package com.example.seminar.controller;
 
+import com.example.seminar.entity.RefreshEntity;
+import com.example.seminar.repository.RefreshRepository;
 import com.example.seminar.util.CookieUtil;
 import com.example.seminar.util.JWTUtil;
 import io.jsonwebtoken.ExpiredJwtException;
@@ -12,11 +14,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequiredArgsConstructor
 public class ReissueController {
 
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
@@ -51,9 +56,25 @@ public class ReissueController {
         String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
         String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);
 
+        refreshRepository.deleteByRefresh(newRefresh);
+        addRefreshEntity(username, newRefresh, 86400000L);
+
         response.setHeader("access", newAccess);
         response.addCookie(CookieUtil.createCookie("refresh", newRefresh));
 
         return new ResponseEntity<>(HttpStatus.OK);
+
     }
+
+    private void addRefreshEntity(String username, String refresh, Long expiredMs) {
+        Date date = new Date(System.currentTimeMillis() + expiredMs);
+
+        RefreshEntity refreshEntity = new RefreshEntity();
+        refreshEntity.setUsername(username);
+        refreshEntity.setRefresh(refresh);
+        refreshEntity.setExpiration(date.toString());
+
+        refreshRepository.save(refreshEntity);
+    }
+
 }
